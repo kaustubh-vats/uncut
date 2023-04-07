5b587e4621752b91e0853eeb9e8caa4aaba67137
Kaustubh Vats <kaustubhvats08@gmail.com>
2023-04-07 14:33:04 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+function isFunction(fn) {
+  return typeof fn === 'function'
+}
+
+function isNativeFunction(fn) {
+  if(!isFunction(fn)) {
+    throw new Error('Expected a function, got ' + typeof fn + ' instead.');
+  } else {
+    return /native code/.test(fn.toString())
+  }
+}
+
+function isNativeObject(obj){
+  return typeof obj === 'object' && /native code/.test(obj.toString())
+}
+
+function getOriginalFunction(fn) {
+  if (isNativeFunction(fn)) {
+    return fn;
+  }
+  const iframe = document.createElement('iframe')
+  iframe.style.display = 'none';
+  document.body.appendChild(iframe);
+  if(iframe.contentWindow && iframe.contentWindow[fn.name]) {
+    const originalFunction = iframe.contentWindow[fn.name];
+    document.body.removeChild(iframe);
+    return originalFunction;
+  }
+  document.body.removeChild(iframe);
+  return null;
+}
+
+function getOriginalFunctionFromString(fn) {
+  const iframe = document.createElement('iframe')
+  iframe.style.display = 'none';
+  document.body.appendChild(iframe);
+  var originalFunction = iframe.contentWindow.eval(fn);
+  document.body.removeChild(iframe);
+  return originalFunction;
+}
+
+function getReturnValues(fn, ...args) {
+  const originalFunction = getOriginalFunction(fn);
+  if(originalFunction) {
+    return originalFunction.apply(null, args);
+  }
+  return null;
+}
+
+function getReturnValuesFromString(fn, ...args) {
+  const originalFunction = getOriginalFunctionFromString(fn);
+  if(originalFunction) {
+    return originalFunction.apply(null, args);
+  }
+  return null;
+}
+
+function getOriginalProtypeMethod(obj, method) {
+  const originalFunction = Object.getPrototypeOf(obj.prototype)[method];
+  return originalFunction;
+}
+
+module.exports = {
+  isFunction,
+  isNativeFunction,
+  isNativeObject,
+  getOriginalFunction,
+  getOriginalFunctionFromString,
+  getReturnValues,
+  getReturnValuesFromString,
+  getOriginalProtypeMethod
+}
